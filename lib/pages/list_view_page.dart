c15e1e84c6aef2dc902f8d823c6d93d030b8af9e
wenqiang <717092390@qq.com>
2019-07-05 00:15:23 +0800

    add images to listView

--- lib/pages/list_view_page.dart
+++ lib/pages/list_view_page.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:hello_flutter/styles/common_style.dart';
 
 class ListViewPage extends StatelessWidget {
+  final List<String> imgList = [
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/12.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/13.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/1816.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/40.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/43.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/9.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,12 +20,14 @@ class ListViewPage extends StatelessWidget {
       body: Container(
         color: CommonStyle.pageBackgroundColor,
         child: ListView.builder(
-          itemCount: 10,
+          itemCount: imgList.length,
           itemBuilder: (context, index) => Container(
                 height: 120,
                 alignment: Alignment.center,
                 margin: EdgeInsets.only(top: 10.0, left: 10, right: 10.0),
                 decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(imgList[index]), fit: BoxFit.cover),
                     color: Colors.white,
                     boxShadow: [BoxShadow(color: Colors.grey)],
                     borderRadius: BorderRadius.circular(10.0)),
