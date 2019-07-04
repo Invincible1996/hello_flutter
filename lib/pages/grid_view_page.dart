6caaa2dd88fd8383e26be13430f418e40a6b4186
wenqiang <717092390@qq.com>
2019-07-05 00:00:27 +0800

    add images

--- lib/pages/grid_view_page.dart
+++ lib/pages/grid_view_page.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
 class GridViewPage extends StatelessWidget {
+  final List<String> imgList = [
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/12.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/13.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/1816.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/40.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/43.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/9.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/12.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/13.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/1816.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/40.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/43.jpg',
+    'https://bigshot.oss-cn-shanghai.aliyuncs.com/wallpaper/9.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,25 +23,30 @@ class GridViewPage extends StatelessWidget {
         title: Text('GridView'),
       ),
       body: Container(
-        padding: EdgeInsets.all(8.0),
+        padding: EdgeInsets.only(left: 10, right: 10, top: 10),
         child: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
+            crossAxisCount: 3,
             mainAxisSpacing: 10.0,
             crossAxisSpacing: 10.0,
             childAspectRatio: 1.0,
           ),
-          itemCount: 10,
+          itemCount: imgList.length,
           itemBuilder: (context, index) => Container(
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(imgList[index]), fit: BoxFit.cover),
                     color: Colors.red,
                     gradient: LinearGradient(
                         colors: [Colors.greenAccent, Colors.yellow],
                         begin: Alignment.bottomLeft,
                         end: Alignment.topRight),
-                    borderRadius: BorderRadius.circular(10.0)),
-                child: Text('GridView'),
+                    borderRadius: BorderRadius.circular(5.0)),
+                child: Text(
+                  'Kobe',
+                  style: TextStyle(color: Colors.white12),
+                ),
               ),
         ),
       ),
