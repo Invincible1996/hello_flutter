f80ccc6af0c81086e6b11f11951b5efd69cd6490
wenqiang <717092390@qq.com>
2019-08-09 16:44:34 +0800

    add bottomNavigationBar page

--- lib/pages/bottom_navigation_bar_page.dart
+++ lib/pages/bottom_navigation_bar_page.dart
@@ -14,6 +14,30 @@ class _BottomNavigationBarPageState extends State<BottomNavigationBarPage> {
         title: Text('BottomNavigationBarPage'),
       ),
       body: Container(),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.person),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
